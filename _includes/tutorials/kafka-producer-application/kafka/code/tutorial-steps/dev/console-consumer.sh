7449a725c52c80809bdb57b1a8db9355b382eded
awalther28 <12937975+awalther28@users.noreply.github.com>
2021-03-31 14:05:36 -0600

    DEVX-noticket: rough POC

--- _includes/tutorials/kafka-producer-application/kafka/code/tutorial-steps/dev/console-consumer.sh
+++ _includes/tutorials/kafka-producer-application/kafka/code/tutorial-steps/dev/console-consumer.sh
@@ -1,5 +1,6 @@
 kafka-console-consumer --topic output-topic \
- --bootstrap-server broker:9092 \
+ --bootstrap-server pkc-4kgmg.us-west-2.aws.confluent.cloud:9092 \
  --from-beginning \
+ --consumer.config stack-configs/java-service-account-*.config \
  --property print.key=true \
  --property key.separator=" : "
